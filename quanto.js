f8e41d58bcc5315bb45927d36ab81eec17d6f326
Lord Terence <76732354+Terence-art@users.noreply.github.com>
2022-11-13 10:49:49 +0100

    Add files via upload

--- quanto.js
+++ quanto.js
@@ -1,5 +1,5 @@
 var a=100; var b=100; var c=100; var d=a+b+c; var e=300;
-var f=Math.ceil(Math.random()*100);
+var f=Math.ceil(Math.random()*100);d=d-f;
 var g=a-f; d=d-g;
 console.log(300-d);
 var h=Math.ceil(Math.random()*100); d=d-h;
